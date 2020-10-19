1e1d5f60bc5aa168d0bae84a4e05811bbd993f16
Soren <soren@aethre.co>
2020-10-19 10:15:14 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='xbox-to-switch',
+      version='1.0',
+      description='Xbox One controller with Nintendo Switch',
+      author='Soren Williams',
+      author_email='soren@aethre.co',
+      url='https://github.com/thrilliams/xbox-to-switch',
+      packages=['inputs'],
+      )
